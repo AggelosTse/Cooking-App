234de8f58479fe9e957b26f88d4fc7e0c2da91d4
AggelosTse <aggu8914@gmail.com>
2025-05-05 22:46:12 +0300

    adding images scroll listview

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,41 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: ImageScroller(),
+    );
+  }
+}
+
+class ImageScroller extends StatelessWidget {
+  const ImageScroller({super.key});
+
+  final List<String> imagePaths = const [
+    'assets/carbonara.jpg',
+    'assets/pancakes.jpg',
+    'assets/steak.jpg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Recipe Gallery")),
+      body: Center(
+        child: SizedBox(
+          height: 200,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: imagePaths.length,
+            itemBuilder: (context, index) {
+              return Container(
+                width: 160,
+                margin: const EdgeInsets.symmetric(horizontal: 8),
+                child: Image.asset(
+                  imagePaths[index],
+                  fit: BoxFit.cover,
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
